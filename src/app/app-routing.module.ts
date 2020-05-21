65f9dedcc14c29d8a45e8cee6552e7d70989438d
Eugene Mazurenko <e.mazurenko@whirl.sg>
2020-05-21 09:07:20 +0300

    add example-page and tooltip draft components and simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExamplePageComponent } from './example-page/example-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'example', 
+  component: ExamplePageComponent
+},{
+  path:'',
+  redirectTo: '/example',
+  pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TooltipComponent } from './components/tooltip/tooltip.component';
+import { ExamplePageComponent } from './example-page/example-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TooltipComponent,
+    ExamplePageComponent
   ],
   imports: [
     BrowserModule,
